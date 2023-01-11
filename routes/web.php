1414738251e727f06ced06f18973493aa0377694
Syed Nasim <syednasim.work@gmail.com>
2023-01-11 18:37:44 +0530

    individual pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    '/rehan',
+    function () {
+        return view('rehan');
+    }
+);
+
+Route::get('/aazim', function () {
+    return view('aazim');
+});
